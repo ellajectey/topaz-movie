abe1de15dc4b397510fe0afc1b37feabe24d3334
Melomey <samuelmelomey@gmail.com>
2024-03-10 16:36:24 +0000

    worked on the login page and series page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,65 @@
-import React from 'react';
-import NavBar from '../components/navBar';
-import Footer from '../components/footer';
+import React from "react";
+import NavBar from "../components/navBar";
+import Footer from "../components/footer";
 
 function Login(props) {
-    return (
-        <>
-        
-            <NavBar/>
-            <p>login page</p>
-            <Footer/>
-        
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <div>
+        <body class="flex justify-center items-center mt-9 ">
+          <div class="bg-white p-10 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-semibold text-center mb-6">Login</h2>
+            <form>
+              <div class="mb-4">
+                <label
+                  class="block text-gray-700 text-sm font-bold mb-2"
+                  for="username"
+                >
+                  Username
+                </label>
+                <input
+                  class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username"
+                  type="text"
+                  placeholder="Enter your username"
+                />
+              </div>
+              <div class="mb-6">
+                <label
+                  class="block text-gray-700 text-sm font-bold mb-2"
+                  for="password"
+                >
+                  Password
+                </label>
+                <input
+                  class="appearance-none border rounded w-full py-2 px-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </div>
+              <div class="flex items-center justify-between">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                >
+                  Sign In
+                </button>
+                <a
+                  class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                  href="#"
+                >
+                  Forgot Password?
+                </a>
+              </div>
+            </form>
+          </div>
+        </body>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Login;
