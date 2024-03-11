7f14829de6817044795c6754b7ff9e1007ed5053
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-03-11 00:24:33 +0000

    started register and search pages

--- src/pages/searchpage.jsx
+++ src/pages/searchpage.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import NavBar from '../components/navBar';
-import Footer from '../components/footer';
+import React from "react";
+import NavBar from "../components/navBar";
+import Footer from "../components/footer";
+import SearchBar from "../components/searchBar";
 
 function Searchpage(props) {
-    return (
-        <>
-        
-            <NavBar/>
-            <p>search page</p>
-            <Footer/>
-        
-        </>
-    );
+  const url = "https://api.themoviedb.org/3/search/movie?api_key=8d14bc5cc17bd609435aa33c0221ce8b&query";
+  console.log(url);
+
+  return (
+    <>
+      <NavBar />
+      <p>search page</p>
+      <div className="text-white h-[90vh] flex justify-center items-center content-center mx-20 bg-contain shadow-lg shadow-indigo-500/40 rounded-md bg-center bg-no-repeat bg-[url('./assets/images/searchpage-header.png')]">
+      <SearchBar/>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Searchpage;
