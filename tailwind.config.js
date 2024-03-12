64b675cd416b58fb6ccc1accfaf3a20315145838
Emmanuella Jectey-Asare <159773530+ellajectey@users.noreply.github.com>
2024-03-12 20:47:02 +0000

    Moviecard ej (#28)
    * worked on the movie card component and made sure it reflected in the movies page
    
    * slight changes to movie card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -7,6 +8,7 @@ module.exports = {
     extend: {
 
     },
+  
   },
   plugins: [],
 }
